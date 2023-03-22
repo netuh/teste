6801dae7c91e8962feeabd982bb1927062c3c659
netuh <waldemar.neto@gmail.com>
2023-03-22 00:07:38 -0300

    update site

--- app.py
+++ app.py
@@ -4,13 +4,17 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
-stateis = "Esta torto!"
+stateis = "00deg"
 
 
 @app.route('/')
-@app.route('/<name>')
 def hello(name=None):
     return render_template('index.html', message=stateis)
+
+
+@app.route('/test')
+def test(name=None):
+    return f'Test com {stateis}'
 
 
 @app.route('/send')
@@ -20,14 +24,14 @@ def send_data():
     az = int(request.args.get('az'))
     global stateis
     if ax == 0 and ay == 0 and az == 1:
-        stateis = "Esta no plano!"
+        stateis = "0deg"
     elif ax == 0 and az == 0 and (ay == 1 or ay == -1):
-        stateis = "Esta em peh!"
+        stateis = "90deg"
     elif ax == 0 and ay == 0 and az == -1:
-        stateis = "Esta de cabeca para baixo!"
+        stateis = "180deg"
     else:
-        stateis = "Esta torto!"
-    return f'Hello, {escape(stateis)}!'
+        stateis = "270deg"
+    return f'alterado para, {escape(stateis)}!'
 
 
 if __name__ == '__main__':
