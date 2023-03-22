293ab85ea763d42cc7fdc880029aa24ed7c4db39
netuh <waldemar.neto@gmail.com>
2023-03-21 21:46:26 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,35 @@
+# save this as app.py
+from flask import Flask, request, render_template
+from markupsafe import escape
+
+app = Flask(__name__)
+
+stateis = "Esta torto!"
+
+
+@app.route('/')
+@app.route('/<name>')
+def hello(name=None):
+    return render_template('index.html', message=stateis)
+
+
+@app.route('/send')
+def send_data():
+    ax = int(request.args.get('ax'))
+    ay = int(request.args.get('ay'))
+    az = int(request.args.get('az'))
+    global stateis
+    if ax == 0 and ay == 0 and az == 1:
+        stateis = "Esta no plano!"
+    elif ax == 0 and az == 0 and (ay == 1 or ay == -1):
+        stateis = "Esta em peh!"
+    elif ax == 0 and ay == 0 and az == -1:
+        stateis = "Esta de cabeca para baixo!"
+    else:
+        stateis = "Esta torto!"
+    return f'Hello, {escape(stateis)}!'
+
+
+if __name__ == '__main__':
+    print("test")
+    app.run(debug=True, host='0.0.0.0', port=80)
